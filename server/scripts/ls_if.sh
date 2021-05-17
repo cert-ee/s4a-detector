c7db66b304e29ed5fee6faffcb9e5584c37801a4
martin <martin@cert.ee>
2021-05-17 14:28:56 +0300

    Fixed NIC status check

--- server/scripts/ls_if.sh
+++ server/scripts/ls_if.sh
@@ -8,7 +8,7 @@
 for x in $( ip link show | sed -nr 's/.*\s([a-z0-9].*):.*\s((DOWN|UP)).*/\1-\2/p' ) ; do
         if=$( echo $x | sed -nr 's/(.*)\-.*/\1/p')
         state=$( echo $x | sed -nr 's/(.*)\-(.*)/\2/p')
-        ip=$( ifconfig $if | grep "inet addr" | cut -d ":" -f2 | awk '{ print $1 }' )
+	ip=$( ip -4 address show dev $if | grep inet | awk '{print $2}' | cut -d'/' -f1 )
 
         echo "{ \"name\": \"$if\" , \"state\": \"$state\", \"ip\": \"$ip\" }"
 done
